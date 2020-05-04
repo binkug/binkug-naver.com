ee48ffbc5f4d8e9f1c8037bca297be69f6c46c61
30404 <30404@DESKTOP-SMDLBOR>
2020-05-04 11:27:46 +0900

    1

---
+++ javaapp0504/src/DaumIPMain.java
@@ -0,0 +1,25 @@
+
+
+
+import java.net.InetAddress;
+
+public class DaumIPMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			InetAddress [] daum = InetAddress.getAllByName("www.daum.net");
+			//InetAddress daum = InetAddress.getByName("www.daum.net");
+			//System.out.println(daum);
+			for(InetAddress imsi : daum) {
+				System.out.println(imsi);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+}
